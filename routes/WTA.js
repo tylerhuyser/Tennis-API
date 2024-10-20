019dc6b67ba406e2d413a22e96dfcd1f969a7738
Tyler Huyser <tyler.huyser@gmail.com>
2024-10-20 14:46:04 -0400

    finished verification for WTA Doubles and Doubles Race Rankings .

--- routes/WTA.js
+++ routes/WTA.js
@@ -5,8 +5,8 @@ const cheerio = require('cheerio');
 // URLS
 const WTA_SINGLES_URL = 'https://api.wtatennis.com/tennis/players/ranked?page=0&pageSize=100&type=rankSingles&sort=asc&name=&metric=SINGLES';
 const WTA_SINGLES_RACE_URL = 'https://api.wtatennis.com/tennis/players/ranked?page=0&pageSize=100&type=RankChampSingles&sort=asc&name=&metric=CHAMPSINGLES'
-const WTA_DOUBLES_URL = 'https://www.wtatennis.com/rankings/doubles'
-const WTA_DOUBLES_RACE_URL = 'https://www.wtatennis.com/rankings/race-doubles'
+const WTA_DOUBLES_URL = 'https://api.wtatennis.com/tennis/players/ranked?page=0&pageSize=100&type=rankDoubles&sort=asc&name=&metric=DOUBLES'
+const WTA_DOUBLES_RACE_URL = 'https://api.wtatennis.com/tennis/players/ranked/champDoubles?page=0&pageSize=1000&type=rank&sort=asc&name='
 
 // WTA RANKINGS
 async function getWTASinglesRankings() {
@@ -44,7 +44,7 @@ router.get('/rankings/singles', async (req, res) => {
       { timeout: 10000 }
     )
   
-      console.log("Inside WTA Singles Call")
+      console.log("Inside WTA Singles Race Call")
   
       // console.log(axiosResponse.data)
 
@@ -52,7 +52,7 @@ router.get('/rankings/singles', async (req, res) => {
 
   } catch (error) {
     console.error(error)
-    res.status(500).json({ error: 'Failed to fetch WTA Singles Rankings' });
+    res.status(500).json({ error: 'Failed to fetch WTA Singles Race Rankings' });
   }
 
 })
@@ -83,159 +83,51 @@ router.get('/rankings/singles-race', async (req, res) => {
 
 })
 
-router.get('/rankings/doubles', async (req, response) => {
+router.get('/rankings/doubles', async (req, res) => {
   
-  let rankings = [];
-  let countries = [];
-  let players = [];
-  let ages = [];
-  let points = [];
-  let tournaments = [];
-  let JSONResponse = [];
+  try { 
 
-  await axios.get(WTA_DOUBLES_URL, {
-    headers:
-      {'User-Agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36'}
-  },
-    { timeout: 2 }
-  ).then((response) => {
+    const axiosResponse = await axios.get(WTA_DOUBLES_URL, {
+      headers:
+        {'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36'}
+    },
+      { timeout: 10000 }
+    )
+  
+      console.log("Inside WTA Doubles Call")
+  
+      // console.log(axiosResponse.data)
 
-    console.log("Inside WTA Doubles Call")
-    console.log(response)
-    console.log(response.data)
+      res.json(axiosResponse.data);
 
-    const $ = cheerio.load(response.data);
-
-    // Scraping rankings
-    $('.js-rankings-list .js-rankings-body .js-player-item-favourite .rankings__cell .rankings__rank').each((i, span) => {
-      rankings.push($(span).text().trim());
-    });
-
-    // Scraping countries
-    $('.js-rankings-list .js-rankings-body .js-player-item-favourite .player td .flag').each((i, img) => {
-      countries.push($(img).attr('alt'));
-    });
-
-    // Scraping names
-    $('.js-rankings-list .js-rankings-body .js-player-item-favourite .rankings__cell--player a').each((i, a) => {
-      players.push($(a).text().trim());
-    });
-
-    // Scraping ages
-    $('.js-rankings-list .js-rankings-body .js-player-item-favourite .rankings__cell--age').each((i, td) => {
-      ages.push($(td).text().trim());
-    });
-
-    // Scraping points
-    $('.js-rankings-list .js-rankings-body .js-player-item-favourite .rankings__cell--points').each((i, td) => {
-      points.push($(td).text().trim());
-    });
-
-    // Scraping tournaments played
-    $('.js-rankings-list .js-rankings-body .js-player-item-favourite .rankings__cell--tournaments').each((i, td) => {
-      tournaments.push($(td).text().trim());
-    });
-
-    for (let i = 0; i < rankings.length; i++){
-      JSONResponse.push({
-        "ranking": rankings[i],
-        "country": countries[i],
-        "player": players[i],
-        "age": ages[i],
-        "points": points[i],
-        "tournaments_played": tournaments[i]
-      })
-    }
-
-    response.json(JSONResponse);
-
-  })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Failed to fetch WTA Doubles Rankings' });
+  }
 })
 
-router.get('/rankings/doubles-race', async (req, response) => {
+router.get('/rankings/doubles-race', async (req, res) => {
   
-  let rankings = [];
-  let countries = [];
-  let players = [];
-  let ages = [];
-  let points = [];
-  let tournaments = [];
-  let JSONResponse = [];
+  try { 
 
-  await axios.get(WTA_DOUBLES_RACE_URL, {
-    headers:
-      {'User-Agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36'}
-  },
-    { timeout: 2 }
-  ).then((response) => {
+    const axiosResponse = await axios.get(WTA_DOUBLES_RACE_URL, {
+      headers:
+        {'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36'}
+    },
+      { timeout: 10000 }
+    )
+  
+      console.log("Inside WTA Doubles Race Call")
+  
+      // console.log(axiosResponse.data)
 
-    console.log("Inside WTA Doubles Race Call")
-    console.log(response)
-    console.log(response.data)
+      res.json(axiosResponse.data);
 
-    const $ = cheerio.load(response.data);
-
-    // Scraping rankings
-    $('.js-rankings-list .js-rankings-body .js-player-item-favourite .rankings__cell .rankings__rank').each((i, span) => {
-      rankings.push($(span).text().trim());
-    });
-
-    // Scraping countries
-    $('.js-rankings-list .js-rankings-body .js-player-item-favourite .player td .flag').each((i, img) => {
-      countries.push($(img).attr('alt'));
-    });
-
-    // Scraping names
-    $('.js-rankings-list .js-rankings-body .js-player-item-favourite .rankings__cell--player a').each((i, a) => {
-      players.push($(a).text().trim());
-    });
-
-    // Scraping ages
-    $('.js-rankings-list .js-rankings-body .js-player-item-favourite .rankings__cell--age').each((i, td) => {
-      ages.push($(td).text().trim());
-    });
-
-    // Scraping points
-    $('.js-rankings-list .js-rankings-body .js-player-item-favourite .rankings__cell--points').each((i, td) => {
-      points.push($(td).text().trim());
-    });
-
-    // Scraping tournaments played
-    $('.js-rankings-list .js-rankings-body .js-player-item-favourite .rankings__cell--tournaments').each((i, td) => {
-      tournaments.push($(td).text().trim());
-    });
-
-    for (let i = 0; i < rankings.length; i++){
-      JSONResponse.push({
-        "ranking": rankings[i],
-        "country": countries[i],
-        "player": players[i],
-        "age": ages[i],
-        "points": points[i],
-        "tournaments_played": tournaments[i]
-      })
-    }
-
-    response.json(JSONResponse);
-
-  })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Failed to fetch WTA Doubles Race Rankings' });
+  }
 })
-
-
-// // WTA singles rankings API response
-// router.get('/rankings/singles', (req, res) => {
-//   axios.get(WTA_RANKINGS_URL).then((response) => {
-//     const $ = cheerio.load(response.data)
-//     console.log(response.data)
-//     // console.log($('.dp-link').text())
-//     res.json({status: 'wta singles!'})
-//   });
-// });
-
-// WTA doubles rankings API response
-router.get('/wta/doubles', (req, res) => {
-});
-
 
 // Export API routes
 module.exports = router;
