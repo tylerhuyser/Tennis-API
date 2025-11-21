dbd5e0cf66d37e89c2747022d61ea77d37acb00c
Tyler Huyser <tyler.huyser@gmail.com>
2025-11-21 14:39:57 -0500

    added condition to serveRoute to rescrape data if rankings array is empty.

--- utils/serveRoute.js
+++ utils/serveRoute.js
@@ -3,16 +3,44 @@ const { readFreshCache, writeCache, readStaleCache } = require('../utils/cache-h
 async function serveRoute(cacheKey, scraper, url, res) {
   try {
     
-    // 1. Serve Fresh Cash
+    // 1. Serve Fresh Cache
     const fresh = readFreshCache(cacheKey);
-    if (fresh) return res.json({ source: 'cache', rankings: fresh.rankings,  lastUpdated: fresh.lastUpdated });
+
+    if (fresh) {
+
+      // 1.a. Check if Fresh Cache contains empty rankings array
+      const rankings = fresh.rankings
+
+      // 1.b. If rankings array is empty, re-scrape
+      if (Array.isArray(rankings) && rankings.length === 0) {
+        console.warn(`[${cacheKey}] Fresh cache contains an empty rankings array — re-scraping.`)
+        const scraped = await scraper(url)
+        writeCache(cacheKey, scraped.rankings);
+        return res.json({
+          source: "cache",
+          rankings: scraped,
+          lastUpdated: new Date().toISOString()
+        });
+      }
+
+      // 1.c. If not, serve JSON response
+      return res.json({ source: 'cache', rankings: fresh.rankings, lastUpdated: fresh.lastUpdated })
+    }
+      
 
     // 2. If Stale, Scrape Data
-    const scraped = await scraper(url); // scraper should return array
-    // 2.a. Write Freshly Scraped Data to Cashe
+    console.log('Stale Data -- Rescraping')
+    const scraped = await scraper(url);
+
+    // 2.a. Write Freshly Scraped Data to Cache
     writeCache(cacheKey, scraped);
 
-    return res.json({ source: 'fresh', rankings: scraped, lastUpdated: new Date().toISOString() });
+    // 2.b. After writing to cache, Serve JSON response
+    return res.json({
+      source: "fresh",
+      rankings: scraped,
+      lastUpdated: new Date().toISOString()
+    });
 
   } catch (err) {
 
@@ -20,7 +48,12 @@ async function serveRoute(cacheKey, scraper, url, res) {
 
     // 3) If error, Fallback to Stale Cache
     const stale = readStaleCache(cacheKey);
-    if (stale) return res.json({ source: 'stale-cache', rankings: stale.rankings, lastUpdated: stale.lastUpdated });
+    if (stale)
+      return res.json({
+        source: 'stale-cache',
+        rankings: stale.rankings,
+        lastUpdated: stale.lastUpdated
+      });
 
     // 4) Otherwise, Serve a Hard Failure
     return res.status(500).json({ error: 'Failed to fetch rankings.' });
