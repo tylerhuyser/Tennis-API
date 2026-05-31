44a770d5925a9d471064737ac1bcbcfbdbbfb8bb
Tyler Huyser <tyler.huyser@gmail.com>
2026-05-31 17:12:25 -0400

    replacing puppetter in fetchBrowser with cloudscraper.

--- utils/fetchBrowser.js
+++ utils/fetchBrowser.js
@@ -1,21 +1,6 @@
-module.exports = async function fetchPageBrowser(url) {
-  const { default: puppeteer } = await import('puppeteer');
-  
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  });
+const cloudscraper = require('cloudscraper');
 
-  try {
-    const page = await browser.newPage();
-    await page.setUserAgent(
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
-    );
-    await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 });
-    await page.waitForSelector('.desktop-table tbody tr', { timeout: 60000 });
-    const html = await page.content();
-    return { data: html };
-  } finally {
-    await browser.close();
-  }
+module.exports = async function fetchPageBrowser(url) {
+  const html = await cloudscraper.get(url);
+  return { data: html };
 };
